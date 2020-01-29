a918b1cb2b2948e242c641083ba027dc3b4dcf64
INTEROCITRON A.I <rajamannar@live.co.uk>
2020-01-29 18:16:03 +0000

    Project files added for Stanford
    Project files added for Stanford

---
+++ DirectoryExplorer/DirectoryExplorer.vb
@@ -0,0 +1,116 @@
+﻿Imports System.IO
+
+'Main UI
+'Building the UI programatically 
+Public Class DirectoryExplorer
+    Inherits Form
+    Private exDirectoryTreeView As ExtendedDirectoryTreeView
+    Private list As New ListView()
+    Private selectedNode As TreeNode
+
+    Sub New()
+        Dim split As New Splitter()
+
+        Text = "Directory Explorer"
+        BackColor = SystemColors.Window
+        ForeColor = SystemColors.WindowText
+
+        ' Add ListView Control
+        ' Detail View
+        list.View = View.Details
+        list.Parent = Me
+        ' Create columns.
+        list.Columns.Add("File Name", 100, HorizontalAlignment.Left)
+        list.Columns.Add("Size (KB)", 100, HorizontalAlignment.Right)
+        list.Columns.Add("Modified", 100, HorizontalAlignment.Left)
+        list.Columns.Add("Attribute", 100, HorizontalAlignment.Left)
+        list.Dock = DockStyle.Fill
+
+        ' Addding the split 
+        split.Parent = Me
+        Split.Dock = DockStyle.Left
+        Split.BackColor = SystemColors.Control
+
+        'Assigning the treeview and docking to the left side of the UI
+        exDirectoryTreeView = New ExtendedDirectoryTreeView()
+        exDirectoryTreeView.Parent = Me
+        exDirectoryTreeView.Dock = DockStyle.Left
+
+        'Adding Event Handler when AfterSelect Event fires
+        AddHandler exDirectoryTreeView.AfterSelect,
+                                AddressOf DirectoryTreeViewOnAfterSelect
+
+        ' Create menu with one item.
+        Menu = New MainMenu()
+        Menu.MenuItems.Add("View")
+        Dim mi As New MenuItem("Refresh",
+                        AddressOf RefreshTreeView, Shortcut.F5)
+        Menu.MenuItems(0).MenuItems.Add(mi)
+    End Sub
+
+
+    Private Sub DirectoryTreeViewOnAfterSelect(ByVal obj As Object,
+                                    ByVal args As TreeViewEventArgs)
+        'Assign the selected node
+        selectedNode = args.Node
+
+        ' Check whether the List contains any items
+        ' If so, clear them first
+        If Not IsNothing(list.Items) Then
+            list.Items.Clear()
+        End If
+
+        'Declare variables for processing the file information
+        Dim currentDirectoryInfo As New DirectoryInfo(selectedNode.FullPath)
+        Dim arrayOfFiles() As FileInfo
+
+        Try
+            ' Get all the file names from the current directory
+            arrayOfFiles = currentDirectoryInfo.GetFiles()
+        Catch
+            Return
+        End Try
+
+        For Each fileInfo In arrayOfFiles
+            ' Create ListViewItem.
+            Dim lvi As New ListViewItem(fileInfo.Name)
+
+            ' Assign ImageIndex based on filename extension.
+            If Path.GetExtension(fileInfo.Name).ToUpper() = ".EXE" Then
+                lvi.ImageIndex = 1
+            Else
+                lvi.ImageIndex = 0
+            End If
+
+            ' Add file length and modified time sub-items.
+            lvi.SubItems.Add(fileInfo.Length.ToString("N0"))
+            lvi.SubItems.Add(fileInfo.LastWriteTime.ToString())
+
+            ' Add attribute subitem.
+            Dim strAttr As String = ""
+            If (fileInfo.Attributes And FileAttributes.Archive) <> 0 Then
+                strAttr &= "A"
+            End If
+            If (fileInfo.Attributes And FileAttributes.Hidden) <> 0 Then
+                strAttr &= "H"
+            End If
+            If (fileInfo.Attributes And FileAttributes.ReadOnly) <> 0 Then
+                strAttr &= "R"
+            End If
+            If (fileInfo.Attributes And FileAttributes.System) <> 0 Then
+                strAttr &= "S"
+            End If
+            lvi.SubItems.Add(strAttr)
+
+            ' Add completed ListViewItem to ListView.
+            list.Items.Add(lvi)
+        Next fileInfo
+
+    End Sub
+
+    'Refresh Event Handler
+    Private Sub RefreshTreeView(ByVal obj As Object, ByVal ea As EventArgs)
+        exDirectoryTreeView.BuildTree()
+    End Sub
+
+End Class
---
+++ DirectoryExplorer/ExtendedDirectoryTreeView.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Drawing
+Imports System.IO
+
+'Inherits from TreeView Control. This Class will populate all the drives, directories and sub-directories
+Public Class ExtendedDirectoryTreeView
+    Inherits TreeView
+    Sub New()
+
+        ' Get images for tree.
+        ' The images are embedded as resource
+        ImageList = New ImageList()
+        ImageList.Images.Add(New Bitmap(Me.GetType(), "HARDDRIVE.png"))
+        ImageList.Images.Add(New Bitmap(Me.GetType(), "CLOSEFOLDER.png"))
+        ImageList.Images.Add(New Bitmap(Me.GetType(), "OPENFOLDER.png"))
+        BuildTree()
+
+    End Sub
+
+    ' BuildTree method builds the initial tree structure and populates with drives and directories as Nodes
+    Sub BuildTree()
+        ' Make disk drives the root nodes. Get the logical drive names
+        Dim arrayOfDrives() As String = Directory.GetLogicalDrives()
+        Dim strDriveName As String
+
+        ' Turn off UI updating and clear tree.
+        ' Adding this one line improved the UI 
+        BeginUpdate()
+        ' I read clearing the tree view is good practice
+        Nodes.Clear()
+
+        ' Loop through all the drives
+        For Each strDriveName In arrayOfDrives
+            Dim driveNode As New TreeNode(strDriveName, 0, 0)
+            ' Add each drive to the Tree as Node
+            Nodes.Add(driveNode)
+            ' Add directories for each drive to the Tree 
+            AddDirectories(driveNode)
+            ' Initial selection of Drive
+            If strDriveName = "C:\" Then
+                'Assign the drive to SelectedNode property of TreeView
+                SelectedNode = driveNode
+            End If
+        Next strDriveName
+        ' Finish Update
+        EndUpdate()
+    End Sub
+
+    ' This method populates all directories of a given node in the TreeNode
+    Private Sub AddDirectories(ByVal currentNode As TreeNode)
+        'Get the current Drive Full Path
+        Dim strDriveFullPath As String = currentNode.FullPath
+        'Get the Root Directory Info
+        Dim rootDirectoryInfo As New DirectoryInfo(strDriveFullPath)
+        Dim arrayOfSubDirectories() As DirectoryInfo
+        Dim currentDirectoryInfo As DirectoryInfo
+
+        'Clear the nodes
+        currentNode.Nodes.Clear()
+
+        ' Check whether Directory Exists 
+        If Not rootDirectoryInfo.Exists Then Return
+
+        Try
+            'Get Sub-Directories, there are any
+            arrayOfSubDirectories = rootDirectoryInfo.GetDirectories()
+        Catch
+            'If any error occurs just return
+            Return
+        End Try
+
+        'Loop through sub directories and get the name of the directory and create node
+        For Each currentDirectoryInfo In arrayOfSubDirectories
+            Dim directoryNode As New TreeNode(currentDirectoryInfo.Name, 1, 2)
+            currentNode.Nodes.Add(directoryNode)
+        Next currentDirectoryInfo
+
+    End Sub
+
+    'Overriding method
+    Protected Overrides Sub OnBeforeExpand(ByVal args As TreeViewCancelEventArgs)
+        MyBase.OnBeforeExpand(args)
+        BeginUpdate()
+        'When a root node expanded , add directores as child nodes
+        For Each currentTreeNode In args.Node.Nodes
+            AddDirectories(currentTreeNode)
+        Next currentTreeNode
+        EndUpdate()
+    End Sub
+End Class
